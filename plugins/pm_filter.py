eefebaee1723459ae160de366503c8ab10a971c7
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-23 18:01:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -861,6 +861,7 @@ async def advantage_spell_chok(msg):
     reply = search.replace(" ", "+")
     reply_markup = InlineKeyboardMarkup([[
      InlineKeyboardButton("🔮IMDB🔮", url=f"https://imdb.com/find?q={reply}"),
+     InlineKeyboardButton("movie.strip()", callback_data=f"spolling#{user}#{k}"),
      InlineKeyboardButton("🪐 Reason", callback_data="reason"),     
      ]]
     )    
